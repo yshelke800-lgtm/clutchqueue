e2a28cf6c345a18721d873ac4344dcf430a1de03
Yash <yash8679@gmail.com>
2026-05-20 05:42:46 +0530

    🐛 Fix: User pre-save hook next() error - use async pattern for Mongoose 7+

--- server/models/User.js
+++ server/models/User.js
@@ -22,10 +22,9 @@ const userSchema = new mongoose.Schema({
   }],
 }, { timestamps: true })
 
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next()
+userSchema.pre('save', async function() {
+  if (!this.isModified('password')) return
   this.password = await bcrypt.hash(this.password, 12)
-  next()
 })
 
 userSchema.methods.matchPassword = async function(enteredPassword) {
